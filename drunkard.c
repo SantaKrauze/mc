dcfcb8e3e08044a98c23adb593bf8d2473e186f1
SantaKrauze <gracjantokarz38@gmail.com>
2020-10-09 16:30:01 +0200

    it walks

---
+++ drunkard.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+void printToFile (int k, double var);
+double varCalc (int x, int n);
+void drunkard (int n, int k);
+
+int main(){
+	int k = 5; //number of drunkards
+	int n = 30000; //number of steps
+	drunkard(n, k);
+	return 0;
+}
+
+void drunkard (int n, int k){
+	int x = 0;
+	float r;
+	for (int i = 1; i <= k; i++){
+		for (int j = 0; j < n; j++){
+			r = (float)rand()/RAND_MAX;
+			if (r > 0.5){
+				x = x + 1;
+				//printf("k:%d val:%f x:%d\n",i,r,x);
+			}
+			else{
+				x = x - 1;
+				//printf("k:%d val:%f x:%d\n",i,r,x);
+			}
+		}
+		printf("k:%d \t Xn:%d \n",i,x);
+		double var = varCalc(x, n);
+		printf("var:%f \n\n",var);
+	}
+}
+
+double varCalc (int xn, int n){
+	double var;
+	double sig;
+	var = xn*xn;
+	sig = sqrt(var);
+	return var;
+}
+
+void printToFile (int k, double var){
+
+}
