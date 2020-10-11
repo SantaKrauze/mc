6b5671e97352473a51841cb9a06440c52a64a785
SantaKrauze <gracjantokarz38@gmail.com>
2020-10-11 18:10:30 +0200

    rearanging files

--- drunkard/main.cpp
+++ drunkard/main.cpp
@@ -2,17 +2,17 @@
 //#include "src/Drunkard.cpp"
 
 int main(){
-	int k = 1000; //number of drunkards
+	int k = 300; //number of drunkards
 	long int n = 1000; //number of steps
 	Drunkard drunkard1(n, k);
 	std::cout<<"var: "<<drunkard1.varCalc()<<std::endl;
-	Drunkard drunkard2(10*n, k);
+	/*Drunkard drunkard2(n, k);
 	std::cout<<"var: "<<drunkard2.varCalc()<<std::endl;
-	Drunkard drunkard3(100*n, k);
+	Drunkard drunkard3(n, k);
 	std::cout<<"var: "<<drunkard3.varCalc()<<std::endl;
-	//Drunkard drunkard4(1000*n, k);
-	//std::cout<<"var: "<<drunkard2.varCalc()<<std::endl;
-	//Drunkard drunkard5(10000*n, k);
-	//std::cout<<"var: "<<drunkard3.varCalc()<<std::endl;
+	Drunkard drunkard4(n, k);
+	std::cout<<"var: "<<drunkard2.varCalc()<<std::endl;
+	Drunkard drunkard5(n, k);
+	std::cout<<"var: "<<drunkard3.varCalc()<<std::endl;*/
 	return 0;
 }
