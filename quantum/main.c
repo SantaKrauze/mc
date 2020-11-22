e8a957bf99f745c13eae29a77c28c6596f448624
SantaKrauze <gracjantokarz38@gmail.com>
2020-11-22 20:36:32 +0100

    energy is changing but not in a good way

--- quantum/main.c
+++ quantum/main.c
@@ -9,7 +9,7 @@ float indexToX(int index);
 //float randomPhi(int index);
 
 int main(){
-	int steps = 1000, index;
+	int steps = 1000000, index;
 	float phiStart = 0.01, dPhi = 0.1;
 	float phiTrial, r, denominator, phi[TSIZE], U = 0, T = 0, numerator, E0;
 	for (int i = 0; i < TSIZE; i++) {
@@ -19,24 +19,28 @@ int main(){
 		denominator =+ phi[i] * phi[i];
 	}
 	for (int i = 1; i < TSIZE-1; i++)T =+ 0.5 * phi[i] * (2 * phi[i] - phi[i-1] - phi[i+1]);
-	T = T + 0.5* ( phi[0] * (2 * phi[0] - phi[1]) + phi[TSIZE-1] * (2 * phi[TSIZE-1] - phi[TSIZE-2]));//potential energy
+	T = T + 0.5 * (phi[0] * (2 * phi[0] - phi[1]) + phi[TSIZE-1] * (2 * phi[TSIZE-1] - phi[TSIZE-2]));//potential energy
 
 	numerator = U + T;
 	E0 = numerator / denominator;//starting energy
 	printf("E0 = %f\n", E0);
 	for (int i = 0; i < steps; i++){
 		r = (float)rand()/RAND_MAX;
-		index = rand() % TSIZE - 1;
+		index = rand() % (TSIZE - 1);
 		phiTrial = phi[index] + (r - 0.5)*dPhi;
 		float deltaPhi = phiTrial - phi[index], dU, dT, x = indexToX(index);
-		if (index == 0) dT = (deltaPhi * deltaPhi - deltaPhi * phi[index+1])/(UNIT * UNIT);
-		else if (index == TSIZE-1) dT = (deltaPhi * deltaPhi - deltaPhi * (-phi[index-1]))/(UNIT * UNIT);
-		else dT = (deltaPhi * deltaPhi - deltaPhi * (phi[index+1] - phi[index-1]))/(UNIT * UNIT);
+		if (index == 0) dT = (deltaPhi * deltaPhi - deltaPhi * phi[index+1]) / (UNIT * UNIT);
+		else if (index == TSIZE-1) dT = (deltaPhi * deltaPhi - deltaPhi * -phi[index-1]) / (UNIT * UNIT);
+		else dT = (deltaPhi * deltaPhi - deltaPhi * (phi[index+1] - phi[index-1])) / (UNIT * UNIT);
 		dU = deltaPhi * deltaPhi * x * x / 2;
 		float newE = (numerator + dU + dT) / (denominator + deltaPhi * deltaPhi);//new energy
-		if (newE < E0) phi[index] = phiTrial;
+		if (newE < E0){
+			phi[index] = phiTrial;
+			E0 = newE;
+		}
 		//printf(" x:%.3f\t phi:%.4f\t phi_trial:%.4f\t newE:%f\n", indexToX(index), phi[index], phiTrial, newE);
 	}
+	printf("E = %f\n", E0);
 	return 0;
 }
 
