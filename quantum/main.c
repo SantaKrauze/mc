38b40745c5a073955f4495b8b03c6760dcb19ce7
SantaKrauze <gracjantokarz38@gmail.com>
2020-11-24 13:15:30 +0100

    E0 now calculating properly

--- quantum/main.c
+++ quantum/main.c
@@ -15,30 +15,34 @@ int main(){
 	for (int i = 0; i < TSIZE; i++) {
 		phi[i] = phiStart;
 		float x = indexToX(i);
-		U = U + (phi[i] * phi[i] * x * x / 2);//potential energy
-		denominator =+ phi[i] * phi[i];
+		U = U + (phi[i] * phi[i] * x * x * 0.5);//potential energy
+		denominator = denominator + phi[i] * phi[i];
 	}
-	for (int i = 1; i < TSIZE-1; i++) T =+ phi[i] * (2 * phi[i] - phi[i-1] - phi[i+1]);
-	printf("U = %f\n", U);
-	T =+ (phi[0] * (2 * phi[0] - phi[1]) + phi[TSIZE-1] * (2 * phi[TSIZE-1] - phi[TSIZE-2])); 
+	for (int i = 1; i < TSIZE-1; i++) T = T + phi[i] * (2 * phi[i] - phi[i-1] - phi[i+1]);
+	T = T + (phi[0] * (2 * phi[0] - phi[1]) + phi[TSIZE-1] * (2 * phi[TSIZE-1] - phi[TSIZE-2])); 
 	T = T * 0.5 / (UNIT * UNIT);//kinetic energy
-	//printf("T = %f\n",T);
 	numerator = U + T;
 	E0 = numerator / denominator;//starting energy
-	printf("E0 = %f\n", E0);
+	printf("E0 = ( %f + %f ) / %f = %f\n", U, T, denominator, E0);
+
 	for (int i = 0; i < steps; i++){
 		r = (float)rand()/RAND_MAX;
 		index = rand() % (TSIZE - 1);
 		phiTrial = phi[index] + (r - 0.5)*dPhi;
-		float deltaPhi = phiTrial - phi[index], dU, dT, x = indexToX(index), deltaPhiPhi = ((phiTrial * phiTrial) - (phi[index] * phi[index]));
-		if (index == 0) dT = (deltaPhiPhi - deltaPhi * phi[index+1]) / (UNIT * UNIT);
-		else if (index == TSIZE-1) dT = (deltaPhiPhi - deltaPhi * phi[index-1]) / (UNIT * UNIT);
-		else dT = (deltaPhiPhi  - deltaPhi * (phi[index+1] - phi[index-1])) / (UNIT * UNIT);
+		float deltaPhi = phiTrial - phi[index];
+		float dU, dT;
+		float x = indexToX(index);
+		float deltaPhiSqr = phiTrial * phiTrial - phi[index] * phi[index];
+
+		if (index == 0) dT = (deltaPhiSqr - deltaPhi * phi[index+1]) / (UNIT * UNIT);
+		else if (index == TSIZE-1) dT = (deltaPhiSqr - deltaPhi * phi[index-1]) / (UNIT * UNIT);
+		else dT = (deltaPhiSqr  - deltaPhi * (phi[index+1] - phi[index-1])) / (UNIT * UNIT);
+
 		dU = deltaPhi * deltaPhi * x * x / 2;
 		float newNumerator = numerator + dU + dT;
-		float newDenominator = denominator + (phiTrial * phiTrial - phi[index] * phi[index]);
+		float newDenominator = denominator + deltaPhiSqr;
 		float newE = newNumerator / newDenominator;//new energy
-		if (newE < E0){
+		if (newE < E0 && newE > 0){
 			phi[index] = phiTrial;
 			E0 = newE;
 			numerator = newNumerator;
