086b8602a34be42d95012087160a82a58f93e261
SantaKrauze <gracjantokarz38@gmail.com>
2020-12-13 17:28:46 +0100

    initial for ising model

---
+++ ising/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	long int steps = 100;
+	int L = 10;
+	int S[L][L];
+	for(int i = 1; i <= steps; i++){
+		for(int j = 0, j < L; j++){
+			for(int k = 0; k < L; k++){
+				//metropolis
+			}
+		}
+		//config analysis, data gathering
+	}
+	//calc average values
+	return 0;
+}
