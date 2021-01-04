9be2883d40e554e6b684e10d9717db2d7ef1a88e
SantaKrauze <gracjantokarz38@gmail.com>
2021-01-04 17:16:37 +0100

    dE calculating fine

--- ising/main.c
+++ ising/main.c
@@ -3,12 +3,40 @@
 
 int main(){
 	long int steps = 100;
-	int L = 10;
+	float T = 0.1;
+	int L = 5;
 	int S[L][L];
-	for(int i = 1; i <= steps; i++){
-		for(int j = 0, j < L; j++){
+	float s;
+	int PI[L], NI[L];
+	for(int i = 1; i < L; i++){
+		NI[i] = i + 1;
+		PI[i] = i - 1;
+	}
+	NI[0] = 1;
+	PI[0] = L;
+	NI[L-1] = 0;
+	double k = 1.380649e-23;
+	for(int i = 0; i < L; i++){//start config
+		for(int j = 0; j < L; j++){
+			s = (float)rand()/RAND_MAX;
+			if(s > 0.5) S[i][j] = 1;
+			else S[i][j] = -1;
+			printf("\t%d",S[i][j]);
+		}
+		printf("\n");
+	}
+	float dE, omega;
+	float R;
+	for(int i = 1; i <= steps; i++){//main loop
+		for(int j = 0; j < L; j++){
 			for(int k = 0; k < L; k++){
-				//metropolis
+				S[j][k] = -S[j][k];
+				dE = -2 * S[j][k] * ( S[NI[j]][k] + S[PI[j]][k] + S[j][NI[k]] + S[j][PI[k]] );
+				//printf("dE%d: %f\n",i,dE);
+				omega = exp(-dE/(k*T));
+				//printf("omega%d: %f\n",i,omega);
+				R = (float)rand()\RAND_MAX;
+				if(R > omega) S[j][k] = -S[j][k];//reversed accept condition
 			}
 		}
 		//config analysis, data gathering
