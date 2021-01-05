0d59b0c51baa40ddf2bf174159ca92f0b9730fd7
SantaKrauze <gracjantokarz38@gmail.com>
2021-01-06 00:03:00 +0100

    not working, as expected

--- ising/main.c
+++ ising/main.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char* argv[]){
 	long int steps = 230000;
-	float T = 0.1;
-	int L = 5;
+	float T = atof(argv[1]);
+	int L = atoi(argv[2]);
 	int S[L][L], PI[L], NI[L];
 	for(int i = 1; i < L; i++){
 		NI[i] = i + 1;
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]){
 	PI[0] = L-1;
 	NI[L-1] = 0;
 	float R;
-	for(int i = 0; i < L; i++){//start config
+	for(int i = 0; i < L; i++){//initial config
 		for(int j = 0; j < L; j++){
 			R = (float)rand()/RAND_MAX;
 			if(R > 0.5) S[i][j] = 1;
@@ -29,38 +29,42 @@ int main(int argc, char* argv[]){
 		B[i] = exp(-d/T);
 		//printf("%d: %e\n",d,B[i]);
 	}
+	int sTrial;
 	float m = 0, mTemp = 0;
 	for(int i = 1; i <= steps; i++){//main loop
 		for(int j = 0; j < L; j++){
 			for(int k = 0; k < L; k++){
 
-				if(S[j][k] == 1) S[j][k] = -1;
-				else S[j][k] = 1;
+				if(S[j][k] == 1) sTrial = -1;
+				else sTrial = 1;
 				
-				dE = -2 * S[j][k] * ( S[NI[j]][k] + S[PI[j]][k] + S[j][NI[k]] + S[j][PI[k]] );
-				omega = exp(-dE/T);
-				//printf("omega%d:\t%.3f\n",i,omega);
-
-				R = (float)rand()/RAND_MAX;
-				if(R > omega){//reversed accept condition
-					if(S[j][k] == 1) S[j][k] = -1;
-					else S[j][k] = 1;
+				dE = -2 * sTrial * ( S[NI[j]][k] + S[PI[j]][k] + S[j][NI[k]] + S[j][PI[k]] );
+				if(dE < 0){
+					S[j][k] = sTrial;
+				}
+				else{
+					omega = exp(-dE/T);
+					//printf("omega%d:\t%.3f\n",i,omega);
+					R = (float)rand()/RAND_MAX;
+					if(R < omega){//reversed accept condition
+						S[j][k] = sTrial;
+					}
 				}
 			}
-		}
-		if(i > 30000){//gathering data for average values
+		}	
+		if(i%100 == 0 && i > 30000){//gathering data for average values
 			for(int i = 0; i < L; i++){
 				for(int j = 0; j < L; j++){
 					mTemp += S[i][j];
 				}
 			}
-			mTemp = mTemp / (L*L);
+			mTemp = abs(mTemp / (L*L));
 			m += mTemp;
 			//printf("%e\n",m);
 		}
 	}
-	m = m / steps;
-	printf("<m> = %.2f\tT* = %.2f\tL = %d\n",m,T,L);
+	m = m / 2000;
+	printf("<m> = %e\tT* = %.2f\tL = %d\n",m,T,L);
 
 	return 0;
 }
